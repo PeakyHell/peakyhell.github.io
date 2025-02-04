ee45dbd61935946e02b3477a63d5e16f671e843b
PeakyHell <louis.vanimpe@student.uclouvain.be>
2025-02-04 17:30:49 +0100

    separate elements creation and cell combination

--- js/project_planner.js
+++ js/project_planner.js
@@ -13,7 +13,7 @@ function itemStackSize(items, item) {
     }
 }
 
-// Create a td element with each element of the elements list as child elements
+// Create a td element with each element of the elements list as child element
 function createCell(elements) {
     let cell = document.createElement('td')
     elements.forEach(element => {
@@ -45,24 +45,20 @@ function fillDatalist(datalist, items) {
 }
 
 
-// Create and setup the item list cell
-function setupItemList(items) {
+// Create the item input and list elements
+function createItemInputList(items) {
     let item_list_input = document.createElement('input')
     item_list_input.setAttribute("list", "item-list")
     let item_list = document.createElement('datalist')
     item_list.id = "item-list"
     fillDatalist(item_list, items)
 
-    item_list_input.addEventListener("input", () => {
-        let stackSize = itemStackSize(items, item_list_input.value)
-    })
-
-    return createCell([item_list_input, item_list])
+    return [item_list_input, item_list]
 }
 
 
-// Create and setup the stack + rest input cell
-function setupStackRestInput() {
+// Create the stack and rest inputs
+function createStackRestInput() {
     let stack_input = document.createElement('input')
     stack_input.type = "number"
     stack_input.setAttribute("min", "0")
@@ -70,35 +66,34 @@ function setupStackRestInput() {
     rest_input.type = "number"
     rest_input.setAttribute("min", "0")
 
-    return createCell([stack_input, "+", rest_input])
+    return [stack_input, rest_input]
 }
 
 
-// Create and setup the amount input cell
-function setupAmountInput() {
+// Create the amount input
+function createAmountInput() {
     let amount_input = document.createElement('input')
     amount_input.type = "number"
 
-    return createCell([amount_input])
+    return amount_input
 }
 
 
 // Create a row to put in the table
-async function createRow(items) {
-
+function createRow(items) {
     let row = document.createElement('tr')
 
 
-    // Item list
-    let item_list_cell = setupItemList(items)
-    
-
-    // Stack and rest input
-    let stack_rest_cell = setupStackRestInput()
+    // Create html elements of row
+    let [item_input, item_list] = createItemInputList(items)
+    let [stack_input, rest_input] = createStackRestInput()
+    let amount_input = createAmountInput()
 
 
-    // Amount input
-    let amount_cell = setupAmountInput()
+    // Create cells
+    let item_list_cell = createCell([item_input, item_list])
+    let stack_rest_cell = createCell([stack_input, "+", rest_input])
+    let amount_cell = createCell([amount_input])
 
 
     // Inserting elements into row
@@ -111,12 +106,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     let items = await minecraftItemsData("1.21.4")
 
     const table = document.querySelector(".planner-config table")
-    table.appendChild(await createRow(items))
+    table.appendChild(createRow(items))
 
     // Add item button add a row each time it's clicked
     const add_item_btn = document.getElementById('add-item-btn')
     add_item_btn.addEventListener('click', async () => {
-        table.appendChild(await createRow(items))
+        table.appendChild(createRow(items))
     })
 
     // Start project button create a table or edit the actual one
